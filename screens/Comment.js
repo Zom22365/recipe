711a363b22555e27b5565382d5e995873a52dcd7
oanhpham <phamoanh22365@gmail.com>
2023-04-22 13:27:33 +0700

    m

--- screens/Comment.js
+++ screens/Comment.js
@@ -1,55 +1,67 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, Button, Platform } from 'react-native';
-
-import DateTimePicker from '@react-native-community/datetimepicker';
-import com from '../data/Comment.json'
-import { SafeAreaView } from 'react-native'
-import { TextInput } from 'react-native'
+import messaging from '@react-native-firebase/messaging';
+import { Alert } from 'react-native';
 
 const Comment = () => {
-    const [comment, setComment] = useState(com)
 
-    const areaComment = comment.map((item, index) => {
-        return (
-            <View key={item.commentId}>
-                <View className="flex-row mb-5 items-center">
-                    <Text className="mr-3 bg-red-200 px-3 py-2 ">{item.userId}</Text>
-                    <Text className="">{item.commet}</Text>
-                </View>
-                <View>
-                    {
-                        item.reply.map((rep, index) => {
-                            return (
-                                <View className="ml-10 mb-4 flex-row items-center" key={rep.commentId}>
-                                    <Text className="mr-3 bg-red-200 px-3 py-2 ">{rep.userId}</Text>
-                                    <Text>{rep.commet}</Text>
-                                </View>
-                            )
-                        })
-                    }
-                    <TextInput
-                        className="ml-10 py-4 px-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-                        placeholder="Nhập bình luận" />
-                </View>
-            </View>
-        )
-    })
+    const requestUserPermission = async () => {
+        const authStatus = await messaging().requestPermission();
+        const enabled =
+            authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+            authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+        if (enabled) {
+            console.log('Authorization status:', authStatus);
+        }
+    }
+
+
+    useEffect(() => {
+        if (requestUserPermission()) {
+            messaging().getToken().then(token => {
+                console.log(token)
+
+            });
+        } else {
+            console.log("Failed token status", authStatus);
+        }
+
+        messaging()
+            .getInitialNotification()
+            .then(async (remoteMessage) => {
+                if (remoteMessage) {
+                    console.log(
+                        'Notification caused app to open from quit state:',
+                        remoteMessage.notification,
+                    );
+                }
+            });
+
+        messaging().onNotificationOpenedApp(async (remoteMessage) => {
+            console.log(
+                'Notification caused app to open from background state:',
+                remoteMessage.notification,
+            );
+        });
+
+        // Register background handler
+        messaging().setBackgroundMessageHandler(async remoteMessage => {
+            console.log('Message handled in the background!', remoteMessage);
+        });
+
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+            Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+        });
+
+        return unsubscribe;
+
+    }, [])
 
     return (
-        <SafeAreaView className="bg-white flex-1">
-            <View className='p-3'>
-                <Text className="mb-10">Ví dụ comment đây</Text>
-                <View>
-                    <TextInput
-                        className="py-4 px-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-
-                        placeholder='Nhập bình luận' />
-                </View>
-                <View>{areaComment}</View>
-
-            </View>
-
-        </SafeAreaView>
+        <View className="flex-1 my-20">
+            <Text>Test</Text>
+        </View>
     )
 
 
---
+++ screens/test.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button, Platform } from 'react-native';
+
+import DateTimePicker from '@react-native-community/datetimepicker';
+import com from '../data/Comment.json'
+import { SafeAreaView } from 'react-native'
+import { TextInput } from 'react-native'
+
+const Comment = () => {
+    const [comment, setComment] = useState(com)
+
+    const areaComment = comment.map((item, index) => {
+        return (
+            <View key={item.commentId}>
+                <View className="flex-row mb-5 items-center">
+                    <Text className="mr-3 bg-red-200 px-3 py-2 ">{item.userId}</Text>
+                    <Text className="">{item.commet}</Text>
+                </View>
+                <View>
+                    {
+                        item.reply.map((rep, index) => {
+                            return (
+                                <View className="ml-10 mb-4 flex-row items-center" key={rep.commentId}>
+                                    <Text className="mr-3 bg-red-200 px-3 py-2 ">{rep.userId}</Text>
+                                    <Text>{rep.commet}</Text>
+                                </View>
+                            )
+                        })
+                    }
+                    <TextInput
+                        className="ml-10 py-4 px-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
+                        placeholder="Nhập bình luận" />
+                </View>
+            </View>
+        )
+    })
+
+    return (
+        <SafeAreaView className="bg-white flex-1">
+            <View className='p-3'>
+                <Text className="mb-10">Ví dụ comment đây</Text>
+                <View>
+                    <TextInput
+                        className="py-4 px-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
+
+                        placeholder='Nhập bình luận' />
+                </View>
+                <View>{areaComment}</View>
+
+            </View>
+
+        </SafeAreaView>
+    )
+
+
+}
+
+
+export default Comment
