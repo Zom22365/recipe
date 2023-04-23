7a6673ab53dd6485ac79157bee60f031542a9f54
interns.trimv <maivantri309@gmail.com>
2023-04-23 21:07:05 +0700

    get token

--- App.js
+++ App.js
@@ -3,9 +3,16 @@ import AppNavigation from './navigation/appNavigation';
 import * as SecureStore from 'expo-secure-store';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getProfile } from './api/apiAcount';
-
+import messaing from "@react-native-firebase/messaging"
+import { Alert } from 'react-native';
 export default function App() {
-
+  const tokenDevice =  async ()=>{
+   return await messaing().getToken()
+  }
+  console.log(tokenDevice)
+  messaing().onMessage(async remoteMessage => {
+    Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+  });
   return (
     <AppNavigation />
   );
